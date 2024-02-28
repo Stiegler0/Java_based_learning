7a039e92341f0988f81bbdab83acf6862fff2789
Stiegler0 <yassinejemlaoui23@gmail.com>
2024-02-28 17:24:28 +0100

    Learned abt overiding of equals() and HashCode(),

--- Collections/src/Livre.java
+++ Collections/src/Livre.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.concurrent.LinkedBlockingDeque;
 public class Livre {
     private String titre;
     private String auteur;
@@ -27,8 +29,23 @@ public class Livre {
         if ((o == null) || getClass() != o.getClass()){
             return false;
         }
-
         Livre livre = (Livre) o;
         return titre.equals(livre.titre) && auteur.equals(livre.auteur);
     }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(titre,auteur);
+   }
+
+   public static void main(String[] args){
+        Livre ob1 = new Livre("T1","A1");
+        Livre ob2 = new Livre("T1","A1");
+
+        System.out.println(ob1.equals(ob2));
+        System.out.println(ob1.hashCode());
+
+        System.out.println(ob2.hashCode());
+
+   }
 }
