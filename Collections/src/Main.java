21cfa5a9f10ffdca151028159512a011eac5869f
Stiegler0 <yassinejemlaoui23@gmail.com>
2024-02-29 02:43:10 +0100

    Main fixed and tested

--- Collections/src/Main.java
+++ Collections/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Livre liv1 = new Livre("Java","yassine");
+        Livre liv2 = new Livre("Py","Yezz");
+        Livre liv3 = liv1;
+
+
+        GestionnaireDeLivres gest = new GestionnaireDeLivres();
+        gest.AjouterLivre(liv1);
+        gest.AjouterLivre(liv2);
+        gest.AjouterLivre(liv3);// mat2ajoutach
+        gest.SupprimerLivre(liv2);
+
+        boolean cont = gest.contientLivre(liv2);
+        System.out.println(cont);
+
+
+        gest.AfficherLivres();
+
+        gest.TrouverLivreParAuteur("yassine");
     }
 }
